f511518f93f9cf6487408b88812136fcb47c52f6
GilbertoMJ <36713462+GilbertoMJ@users.noreply.github.com>
2018-11-30 14:42:55 -0200

    Update ficha_del.php

--- ficha_del.php
+++ ficha_del.php
@@ -1,9 +1,9 @@
 <?php include 'config.php';
-  $sql = mysqli_query($conexao, "DELETE FROM cadastro WHERE codigo =".$_POST['codigo_'])
-  or print(mysqli_error($conexao))
-
-  if($sql == 1)
-	echo 'Dados deletados!';
-else
-	echo 'Erro!';
+	$sql = mysqli_query($conexao, "DELETE FROM cadastro WHERE codigo =".$_POST['codigo_']) 
+	or print(mysqli_error($conexao))
+		
+	if($sql == 1)
+		echo 'Dados deletados!';
+ 	else
+		echo 'Erro!';
 ?>
